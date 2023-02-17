bcd405bcb778ea7c4c25cb2577a0363d574abb68
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-17 03:50:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - main block 
- * Return: o
+ * main - prints "Programming is like building a mutilingual puzzle, followed by a new line"
+ * return 0.
  */
+
 int main(void)
+
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
