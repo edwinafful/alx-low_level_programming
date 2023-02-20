09ffec7967ca17462bef8a06c56a79e1c55d6c90
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-20 06:52:43 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry level application
+ * Return: Always (0) (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar (n);
+	putchar ('\n');
+		return (0);
+}
