dab8ab548a08b67bfc8a104e8e26f0ac0d862cda
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-20 08:07:57 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry level application
+ * Return: Always (0) (Sucess)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		if (m > n)
+		{
+			putchar (n);
+			putchar (m);
+			if (n != 56 || m != 57)
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
