a2f8577eadf611ac6d877b3509b045690a53655a
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-20 06:36:01 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry level application
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++);
+	printf("%d", n);
+	printf("\n");
+return (0);
+}
