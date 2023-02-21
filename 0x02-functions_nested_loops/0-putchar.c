0ce3809910dd4285cefbb61ff8bdfd365fc7264a
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-21 06:21:46 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ * Return: Always (0) (Sucess)
+ */
+
+char main(void)
+{
+	char ch = "_putchar";
+
+printf("%s \n", ch);
+return (0);
+}
