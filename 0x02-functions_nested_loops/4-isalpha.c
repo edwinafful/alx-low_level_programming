c9950db2b21cfda8b944da693da3d3955d45566e
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-22 07:55:36 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: is a member to be checked
+ * Return: 1 if is alphabetic character, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
