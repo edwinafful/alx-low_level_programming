450cf4f596ef50e319796138e395b82aed6feaec
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-22 06:39:23 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * print_alphabet - print all alphabelt in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+	return (0);
+}
