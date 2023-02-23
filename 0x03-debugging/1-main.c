aff1bba5154d9fc5452f1656f714071eb386d87b
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-23 05:48:30 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-printf("Infinite loop incoming :(\n");/* Infinite loop incoming */
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 
@@ -18,7 +18,7 @@ while (i < 10)
 putchar(i);
 }
 
-printf("Infinite loop avoided! \\o//\n");
+printf("Infinite loop avoided! \\o\/\n");
 
 return (0);
 }
