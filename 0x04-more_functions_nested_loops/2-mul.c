1f342720e1a87a3c345c13a6128c923958e46e93
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-24 06:37:00 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first interger
+ * @b: second integer
+ * Return: Always 0
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
+
