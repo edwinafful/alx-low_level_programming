3f4a469aad4e0f8f0828e49ecf1a0d05858e6ed1
Edwin Kofi Afful <edwinafful17@gmail>
2023-02-27 19:32:46 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strcpy - Entry level
+ * @dest: copy to
+ * @src: copy from
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src);
+{
+	int I = 0;
+
+	int X = 0;
+
+	while (*(src + I) != '\0')
+	{
+		I++;
+	}
+	for (; x < ; x++)
+	{
+		dest[X] = src[X]
+	}
+	dest[I] = '\0';
+	return (dest);
+}
