d48a2bf5a69201a868501f8f35d417ebf10dc0e6
Edwin Kofi Afful <edwinafful17@gmail>
2023-03-02 19:22:38 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -19,11 +19,11 @@ void print_buffer(char *b, int size)
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
-		printf("%08*:", o);
+		printf("%08x:", o);
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
-				printf("%02*", *(b + o + i));
+				printf("%02x", *(b + o + i));
 			else
 				printf(" ");
 			if (i % 2)
