1f595f29db5d335f883b9c897d621b748e3f1f56
Edwin Kofi Afful <edwinafful17@gmail>
2023-03-08 19:39:41 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: input string
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	_puts_recursion("Puts with recursion\n");
+	return (0);
+}
