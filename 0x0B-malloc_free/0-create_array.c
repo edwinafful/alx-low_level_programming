72b4fc481e6f32c49fe9b5347914419246288216
Edwin Kofi Afful <edwinafful17@gmail>
2023-03-14 19:56:04 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == Null)
-		return (Null);
+	if (size == 0 || str == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
